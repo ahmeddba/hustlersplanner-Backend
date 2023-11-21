a8a2740fa319d93c1f72d220cb308d0309167224
ahmeddba <ahmed.benabid@esprit.tn>
2023-11-21 10:59:26 +0100

    update server

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const connect = require('./ConfigDB/database')
 const app = express();
 app.use(express.json());
+
 require('dotenv').config();
 
-app.use((req,res) => {
-    res.send("API is running...")
-})
+
 
 const PORT = process.env.PORT || 7888;
 
@@ -19,3 +18,6 @@ connect();
 app.use('/api/tasks', require('./Routes/taskRoute'));
 app.use('/api/users', require('./Routes/UserRoute'));
 
+app.use((req,res) => {
+    res.send("API is running...")
+})
